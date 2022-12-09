f41c1c88fc258fb3d6718446187f8eaa6f15ff49
alexandremax1m <119704433+alexandremax1m@users.noreply.github.com>
2022-12-09 22:00:26 +0100

    Update _autosubplot.py

--- _autosubplot.py
+++ _autosubplot.py
@@ -26,14 +26,14 @@ for i in index:
 			pointer0,pointer1 = c_int * (-(int(len(_2ddata[0])))*-1), c_int * (-(int(len(_2ddata[1])))*-1)
 			data0,data1 = pointer0(*(tuple(_2ddata[0]))),pointer1(*(tuple(_2ddata[1])))
 			b = int(input(f"Choose Projection Style:: 1:aitoff,2:hammer,3:lambert,4:mollweide,5:polar:6:rectilinear,7:none "))
+			c = plt.subplot(int(f"{cn}{rn}{i}"),projection=_projections[b])
+			c.set_xlabel(xlab)
+			c.set_ylabel(ylab)
 			while True:
 						a = int(input(f"Choose Plotting Style:: 1:Plot,2:Scatter,3:Bar,4:Stem,5:Step,6:Finish "))
-						if not a == int(6):
-							c = plt.subplot(int(f"{cn}{rn}{i}"),projection=_projections[b])
-							c.set_xlabel(xlab)
-							c.set_ylabel(ylab)
-							plotdict={-1*-1:c.plot([i for i in data0],[i for i in data1]),-2*-1:c.scatter([i for i in data0],[i for i in data1]),-3*-1:c.bar([i for i in data0],[i for i in data1]),-4*-1:c.stem([i for i in data0],[i for i in data1]),-5*-1:c.step([i for i in data0],[i for i in data1])}
-							plotdict[a]	
+						if not a == int(6):							
+							plotdict={-1*-1:c.plot,-2*-1:c.scatter,-3*-1:c.bar,-4*-1:c.stem,-5*-1:c.step}
+							plotdict[a]([i for i in data0],[i for i in data1])
 						elif a == int(6):
 							del _2ddata,pointer0,pointer1,data0,data1,a,b
 							break
@@ -49,14 +49,14 @@ for i in index:
 			pointer0,pointer1,pointer2 = c_int * (-(int(len(_3ddata[0])))*-1),c_int * (-(int(len(_3ddata[1])))*-1),c_int * (-(int(len(_3ddata[1])))*-1)
 			data0,data1,data2 = pointer0(*(tuple(_3ddata[0]))),pointer1(*(tuple(_3ddata[1]))),pointer2(*(tuple(_3ddata[2])))
 			b = int(input(f"Choose Projection Style:: 1:aitoff,2:hammer,3:lambert,4:mollweide,5:polar:6:rectilinear,7:none "))
+			c = plt.subplot(int(f"{cn}{rn}{i}"),projection=_projections[b])
+			c.set_xlabel(xlab)
+			c.set_ylabel(ylab)
 			while True:
 						a = int(input(f"Choose Plotting Style:: 1:Stackplot,2:FillBetween,3:Finish "))
-						if not a == int(3):
-							c = plt.subplot(int(f"{cn}{rn}{i}"),projection=_projections[b])
-							c.set_xlabel(xlab)
-							c.set_ylabel(ylab)
-							plotdict = {-1*-1:c.stackplot([i for i in data0],[i for i in data1],[i for i in data2]),-2*-1:c.fill_between([i for i in data0],[i for i in data1],[i for i in data2])}
-							plotdict[a]
+						if not a == int(3):				
+							plotdict = {-1*-1:c.stackplot,-2*-1:c.fill_between}
+							plotdict[a]([i for i in data0],[i for i in data1],[i for i in data2])
 						elif a == int(3):
 							del _3ddata,pointer0,pointer1,pointer2,data0,data1,data2,a,b
 							break
