6eb0e66804fc266d09232b1632eef82e27930576
alexandremax1m <119704433+alexandremax1m@users.noreply.github.com>
2022-12-10 02:26:37 +0100

    Delete mytestnums.py

--- mytestnums.py
+++
@@ -1,5 +0,0 @@
-def thismuchto10(oneobject):
-	return (c_int * (-1*-int(len(oneobject))))(*(tuple([x for x in bytes([x for x in range((-1*-int(len(oneobject))))])])))
-
-def thismuchto10n(n):
-	return (c_int * (-1*-int(n)))(*(tuple([x for x in bytes([x for x in range(-1*-int(n))])])))
