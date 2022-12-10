df0396fde61ea4208cbe335a3b6117a195d035a0
alexandremax1m <119704433+alexandremax1m@users.noreply.github.com>
2022-12-10 12:28:02 +0100

    Update _mytestnums.py

--- _mytestnums.py
+++ _mytestnums.py
@@ -1,7 +1,7 @@
 from ctypes import *
 
 def thismuchto10(oneobject):
-	return (c_int * (-1*-int(len(oneobject))))(*(tuple([x for x in bytes([x for x in range((-1*-int(len(oneobject))))])])))
+	return (c_int * (-1*-int(len(oneobject))))(*(tuple(bytes([x for x in range((-1*-int(len(oneobject))))]))))
 
 def thismuchto10n(n):
-	return (c_int * (-1*-int(n)))(*(tuple([x for x in bytes([x for x in range(-1*-int(n))])])))
+	return (c_int * (-1*-int(n)))(*(tuple(bytes([x for x in range(n)]))))
