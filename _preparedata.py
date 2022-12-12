7df2b2da3e1665ff3d1720f504deb9611eea10f4
alexandremax1m <119704433+alexandremax1m@users.noreply.github.com>
2022-12-12 19:55:29 +0100

    Update _preparedata.py

--- _preparedata.py
+++ _preparedata.py
@@ -1,27 +1,33 @@
 from ctypes import *
 from _pyintoctp import cintn
 
-def _prepare2d():
+def _prepare2d(*_2dlist):
 	_2dlist = [[],[]]
 	for i in cintn(int(input('How Many Values For XY Axes Sir :: '))):
-		_2dlist[0].append(int(input(f'Your Values X {i} Sir :: ')))
-		_2dlist[1].append(int(input(f'Your Values Y {i} Sir :: ')))
-	_2dlist[0],_2dlist[1] = bytes(_2dlist[0]),bytes(_2dlist[1])
-	return _2dlist
+		_2dlist[-0*-1].append(int(input(f'Your Values X {i} Sir :: ')))
+		_2dlist[-1*-1].append(int(input(f'Your Values Y {i} Sir :: ')))
+	_2dlist[-0*-1],_2dlist[-1*-1] = bytes(_2dlist[-0*-1]),bytes(_2dlist[-1*-1])
+	data0,data1 = (c_int*(-(int(len(_2dlist[-0*-1])))*-1))(*(tuple(_2dlist[-0*-1]))),(c_int*(-(int(len(_2dlist[-1*-1])))*-1))(*(tuple(_2dlist[-1*-1])))
+	return {-1*-1:_2dlist,-0*-1:{-1*-1:data1,-0*-1:data0}}
 
-def _prepare3d():
+def _prepare3d(*_3dlist):
 	_3dlist = [[],[],[]]
 	for i in cintn(int(input('How Many Values For XYZ Axes Sir :: '))):
 		_3dlist[0].append(int(input(f'Your Values X {i} Sir :: ')))
 		_3dlist[1].append(int(input(f'Your Values X {i} Sir :: ')))
 		_3dlist[2].append(int(input(f'Your Values X {i} Sir :: ')))
-	_3dlist[0],_3dlist[1],_3dlist[2] == bytes(_3dlist[0]),bytes(_3dlist[1]),bytes(_3dlist[2])
-	return _3dlist
+	_3dlist[0],_3dlist[1],_3dlist[2] = bytes(_3dlist[0]),bytes(_3dlist[1]),bytes(_3dlist[2])
+	data0,data1,data2 = (c_int*(-(int(len(_3dlist[-0*-1])))*-1))(*(tuple(_3dlist[-0*-1]))),(c_int*(-(int(len(_3dlist[-1*-1])))*-1))(*(tuple(_3dlist[-1*-1]))),(c_int*(-(int(len(_3dlist[-2*-1])))*-1))(*(tuple(_3dlist[-2*-1])))
+	return {-1*-1:_3dlist,-0*-1:{-2*-1:data0,-1*-1:data1,-0*-1:data0}}
 
 def _prepare2dfrom(alist):
-	return [bytes(alist[0]),bytes(alist[1])]
+	alist[-0*-1],alist[-1*-1] = bytes(alist[-0*-1]),bytes(alist[-1*-1])
+	data0,data1 = (c_int*(-(int(len(alist[-0*-1])))*-1))(*(tuple(alist[-0*-1]))),(c_int*(-(int(len(alist[-1*-1])))*-1))(*(tuple(alist[-1*-1])))
+	return {-1*-1:alist,-0*-1:{-1*-1:data1,-0*-1:data0}}
 
 def _prepare3dfrom(alist):
-	return [bytes(alist[0],alist[1],alist[2])]
+	data0,data1,data2 = (c_int*(-(int(len(alist[-0*-1])))*-1))(*(tuple(alist[-0*-1]))),(c_int*(-(int(len(alist[-1*-1])))*-1))(*(tuple(alist[-1*-1]))),(c_int*(-(int(len(alist[-2*-1])))*-1))(*(tuple(alist[-2*-1])))
+	alist[-0*-1],alist[-1*-1],alist[-2*-1] = bytes(alist[-0*-1]),bytes(alist[-1*-1]),bytes(alist[-2*-1])
+	return {-1*-1:alist,-0*-1:{-2*-1:data0,-1*-1:data1,-0*-1:data0}}
 
 preparedatadict = {-4*-1:_prepare2dfrom,-3*-1:_prepare3dfrom,-2*-1:_prepare3d,-1*-1:_prepare2d}
