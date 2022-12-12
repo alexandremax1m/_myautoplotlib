5079b2dc63234217b5ba2e1f2c2cbfb336ae47a8
alexandremax1m <119704433+alexandremax1m@users.noreply.github.com>
2022-12-12 14:24:21 +0100

    Update _preparedata.py

--- _preparedata.py
+++ _preparedata.py
@@ -2,22 +2,20 @@ from ctypes import *
 from _pyintoctp import cintn
 
 def _prepare2d(*_2dlist):
-	if not _2dlist:
-		_2dlist = [[],[]]
-		for i in cintn(int(input('How Many Values For XY Axes Sir :: '))):
-			_2dlist[0].append(int(input(f'Your Values X {i} Sir :: ')))
-			_2dlist[1].append(int(input(f'Your Values Y {i} Sir :: ')))
-		_2dlist[0],_2dlist[1] = bytes(_2dlist[0]),bytes(_2dlist[1])
-		return _2dlist
+	_2dlist = [[],[]]
+	for i in cintn(int(input('How Many Values For XY Axes Sir :: '))):
+		_2dlist[0].append(int(input(f'Your Values X {i} Sir :: ')))
+		_2dlist[1].append(int(input(f'Your Values Y {i} Sir :: ')))
+	_2dlist[0],_2dlist[1] = bytes(_2dlist[0]),bytes(_2dlist[1])
+	return _2dlist
 
 def _prepare3d(*_3dlist):
-	if not _3dlist:
-		_3dlist = [[],[],[]]
-		for i in cintn(int(input('How Many Values For XYZ Axes Sir :: '))):
-			_3dlist[0].append(int(input(f'Your Values X {i} Sir :: ')))
-			_3dlist[1].append(int(input(f'Your Values X {i} Sir :: ')))
-			_3dlist[2].append(int(input(f'Your Values X {i} Sir :: ')))
-		_3dlist[0],_3dlist[1],_3dlist[2] == bytes(_3dlist[0]),bytes(_3dlist[1]),bytes(_3dlist[2])
-		return _3dlist
+	_3dlist = [[],[],[]]
+	for i in cintn(int(input('How Many Values For XYZ Axes Sir :: '))):
+		_3dlist[0].append(int(input(f'Your Values X {i} Sir :: ')))
+		_3dlist[1].append(int(input(f'Your Values X {i} Sir :: ')))
+		_3dlist[2].append(int(input(f'Your Values X {i} Sir :: ')))
+	_3dlist[0],_3dlist[1],_3dlist[2] == bytes(_3dlist[0]),bytes(_3dlist[1]),bytes(_3dlist[2])
+	return _3dlist
 
 preparedatadict = {-2*-1:_prepare3d,-1*-1:_prepare2d}
