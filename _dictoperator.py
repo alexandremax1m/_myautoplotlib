ba3989572172fa487100e451f25b1695cd91f248
alexandremax1m <119704433+alexandremax1m@users.noreply.github.com>
2022-12-12 21:29:32 +0100

    Update _dictoperator.py

--- _dictoperator.py
+++ _dictoperator.py
@@ -4,3 +4,7 @@ from _myplotdicts import _pyplotobj,_objmeth2,_objmeth3
 a = preparedatadict[-1*-1]()
 _objmeth2[-4*-1]([i for i in a[-1*-1][-0*-1]],[i for i in a[-1*-1][-1*-1]])
 _pyplotobj[-1*-1].show()
+
+## import 
+## or ipython,anaconda
+## or interactively // ##
