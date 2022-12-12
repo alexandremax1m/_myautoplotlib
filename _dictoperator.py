919089d3ea16272bb129703edb0d1997fefd2605
alexandremax1m <119704433+alexandremax1m@users.noreply.github.com>
2022-12-12 21:25:00 +0100

    Add files via upload

---
+++ _dictoperator.py
@@ -0,0 +1,6 @@
+from _preparedata import preparedatadict
+from _myplotdicts import _pyplotobj,_objmeth2,_objmeth3
+
+a = preparedatadict[-1*-1]()
+_objmeth2[-4*-1]([i for i in a[-1*-1][-0*-1]],[i for i in a[-1*-1][-1*-1]])
+_pyplotobj[-1*-1].show()
