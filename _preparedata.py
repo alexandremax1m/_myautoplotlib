fe0b8173044fe4bebacd62d6b9ef81caf4f07533
alexandremax1m <119704433+alexandremax1m@users.noreply.github.com>
2022-12-12 14:31:20 +0100

    Update _preparedata.py

--- _preparedata.py
+++ _preparedata.py
@@ -1,7 +1,7 @@
 from ctypes import *
 from _pyintoctp import cintn
 
-def _prepare2d():
+def _prepare2d(*_2dlist):
 	_2dlist = [[],[]]
 	for i in cintn(int(input('How Many Values For XY Axes Sir :: '))):
 		_2dlist[0].append(int(input(f'Your Values X {i} Sir :: ')))
@@ -9,7 +9,7 @@ def _prepare2d():
 	_2dlist[0],_2dlist[1] = bytes(_2dlist[0]),bytes(_2dlist[1])
 	return _2dlist
 
-def _prepare3d():
+def _prepare3d(*_3dlist):
 	_3dlist = [[],[],[]]
 	for i in cintn(int(input('How Many Values For XYZ Axes Sir :: '))):
 		_3dlist[0].append(int(input(f'Your Values X {i} Sir :: ')))
@@ -18,4 +18,10 @@ def _prepare3d():
 	_3dlist[0],_3dlist[1],_3dlist[2] == bytes(_3dlist[0]),bytes(_3dlist[1]),bytes(_3dlist[2])
 	return _3dlist
 
-preparedatadict = {-2*-1:_prepare3d,-1*-1:_prepare2d}
+def _prepare2dfrom(alist):
+	return [bytes(alist[0]),bytes(alist[1])]
+
+def _prepare3dfrom(alist):
+	return [bytes(alist[0],alist[1],alist[2])]
+
+preparedatadict = {-4*-1:_prepare2dfrom,-3*-1:_prepare3dfrom,-2*-1:_prepare3d,-1*-1:_prepare2d}
