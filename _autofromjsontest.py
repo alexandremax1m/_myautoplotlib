6da81a5bf27b05d3bf63335644cf4eb962330778
alexandremax1m <119704433+alexandremax1m@users.noreply.github.com>
2022-12-14 16:52:39 +0100

    Update _autofromjsontest.py

--- _autofromjsontest.py
+++ _autofromjsontest.py
@@ -11,12 +11,11 @@ jsonfile = json.load(open(aa[int(input(f'{a} Choose File Sir :: {aa} '))],'r'))
 keyslist = [key for key in jsonfile]
 keys,chosenkeys,data = {},[],[]
 keys.update({x:keyslist[x] for x in range(len(keyslist))})
-
-for x in range(2):
-	print(keys)
-	chosenkeys.append(keys[int(input('Your Key Number Sir :: '))])
-	data.append([i for i in jsonfile[chosenkeys[x]]])
-
+print(keys)
+chosenkeys.append(keys[int(input('Your Key Number Sir :: '))])
+chosenkeys.append(keys[int(input('Your Key Number Sir :: '))])
+data.append(([i for i in jsonfile[chosenkeys[0]]]))
+data.append(([i for i in jsonfile[chosenkeys[0]]]))
 datakeys = [key for key in data[0][0]]
 
 plt.plot(
