69802c4de550129b9aaebc33be6a00f2f0f1b99f
alexandremax1m <119704433+alexandremax1m@users.noreply.github.com>
2022-12-14 17:04:18 +0100

    Update _autofromjsontest.py

--- _autofromjsontest.py
+++ _autofromjsontest.py
@@ -11,9 +11,8 @@ jsonfile = json.load(open(aa[int(input(f'{a} Choose File Sir :: {aa} '))],'r'))
 keyslist = [key for key in jsonfile]
 keys,chosenkeys,data = {},[],[]
 keys.update({x:keyslist[x] for x in range(len(keyslist))})
-print(keys)
-chosenkeys.append(keys[int(input('Your Key Number Sir :: '))])
-chosenkeys.append(keys[int(input('Your Key Number Sir :: '))])
+chosenkeys.append(keys[int(input(f'{keys}    Your Key Number Sir :: '))])
+chosenkeys.append(keys[int(input(f'{keys}    Your Key Number Sir :: '))])
 data.append(([i for i in jsonfile[chosenkeys[0]]]))
 data.append(([i for i in jsonfile[chosenkeys[1]]]))
 datakeys = [key for key in data[0][0]]
